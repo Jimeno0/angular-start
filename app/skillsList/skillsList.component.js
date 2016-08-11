7a97965f5398f1c522541e57cfe9e7a8db3c338a
Jimeno0 <carlospj@icloud.com>
2016-08-11 16:35:28 +0200

    fixing indentation

--- app/skillsList/skillsList.component.js
+++ app/skillsList/skillsList.component.js
@@ -2,14 +2,14 @@ angular.
 	module('skillsList').
 		component('skillsList',{
 
-			templateUrl:'app/skillsList/skillsList.template.html',
+			templateUrl: 'app/skillsList/skillsList.template.html',
 
-				controller: function phoneListController($http){
-					var self = this;
-					self.query = 'webDev';
-					$http.get('app/data/skills.json').then(function(response){
-						self.skills = response.data;
-					});
-				}
+			controller: function phoneListController($http) {
+				var self = this;
+				self.query = 'webDev';
+				$http.get('app/data/skills.json').then(function(response){
+					self.skills = response.data;
+				});
+			}
 
 		});
